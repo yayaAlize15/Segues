1858597b9b44d4600ff1872f294cbbc1e0ea12e2
yayam15 <40362526+yayam15@users.noreply.github.com>
2021-07-29 12:09:16 -0400

    1st commit

---
+++ Intro-to-Segues/Intro-to-Segues/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Intro-to-Segues
+//
+//  Created by Yazmyn Aguilar on 7/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
